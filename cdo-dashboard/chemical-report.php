801acfd7235ebc20e3386408abfef6e7fe17527d
Bheem Chand <bheemchand8126@gmail.com>
2026-08-26 21:24:29 +0530

    added error code

--- cdo-dashboard/chemical-report.php
+++ cdo-dashboard/chemical-report.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'auth.php';
 
 $fromDate = $_GET['from_date'] ?? date('Y-m-d', strtotime('-6 days'));

e937de61a81198fcc230c607f92761e3dbaf0590
Bheem Chand <bheemchand8126@gmail.com>
2026-08-27 15:10:01 +0530

    updated ui of score card 2

--- cdo-dashboard/intensive_scorecard_2.php
+++ cdo-dashboard/intensive_scorecard_2.php
@@ -668,13 +668,18 @@ include 'sidebar.php';
                             <table class="cts-table">
                                 <thead>
                                     <tr>
-                                        <th class="col-sn">SL<br>No.</th>
-                                        <th class="col-desc">Description</th>
-                                        <th class="col-slot">Area / Part</th>
+                                        <th rowspan="2" class="col-sn">SL<br>No.</th>
+                                        <th rowspan="2" class="col-desc">Description</th>
+                                        <th rowspan="2" class="col-slot">Area / Part</th>
+                                        <?php for ($c = 1; $c <= 24; $c++): ?>
+                                            <th class="col-c th-c-head">C<?= $c ?></th>
+                                        <?php endfor; ?>
+                                        <th rowspan="2" class="col-ts">T.S.</th>
+                                    </tr>
+                                    <tr>
                                         <?php for ($c = 0; $c < 24; $c++): ?>
                                             <th class="col-c th-c-num"><?= htmlspecialchars($sheet['coach_numbers'][$c] ?? '') ?></th>
                                         <?php endfor; ?>
-                                        <th class="col-ts">T.S.</th>
                                     </tr>
                                 </thead>
                                 <tbody>
